45402c053af1ecf55f7fb18877ef02eed9977ecf
edoomm <edoomm8@gmail.com>
2020-12-15 18:37:48 -0600

    bottom nav bar del menu principal implementado

--- app/src/main/java/com/example/pickingapp/Menu.java
+++ app/src/main/java/com/example/pickingapp/Menu.java
@@ -1,14 +1,33 @@
 package com.example.pickingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class Menu extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Menu extends AppCompatActivity implements BottomNavigationView.OnNavigationItemReselectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_menu, new MenuFragment()).commit();
+
+        bottomNavigationView.setOnNavigationItemReselectedListener(this);
+    }
+
+    @Override
+    public void onNavigationItemReselected(@NonNull MenuItem item) {
+        Toast.makeText(getApplicationContext(), "Hasta pronto", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
