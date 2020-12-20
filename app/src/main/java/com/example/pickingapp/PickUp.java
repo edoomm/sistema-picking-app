eec8c74a9750b06fae64f92914d13a9167908597
ErickRazielHernandez <hernandezmagallonerick@gmail.com>
2020-12-19 20:32:36 -0600

    Se añadió la pantalla de almacen

--- app/src/main/java/com/example/pickingapp/PickUp.java
+++ app/src/main/java/com/example/pickingapp/PickUp.java
@@ -4,15 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PickUp extends AppCompatActivity {
 
