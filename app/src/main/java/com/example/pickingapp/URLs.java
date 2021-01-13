30e1bee2a4266a4c28c1bf114343ac17a6277ecf
Donaldo Ayala <ayala.segoviano.donaldo@gmail.com>
2021-01-13 02:14:39 -0600

    Fixing URL content

--- app/src/main/java/com/example/pickingapp/URLs.java
+++ app/src/main/java/com/example/pickingapp/URLs.java
@@ -3,6 +3,8 @@ package com.example.pickingapp;
 public class URLs {
     private static final String IP = "192.168.1.69"; // cambiará dependiendo de la IP que tenga su computadora, cuando se modifique no añadan cambios en el git pls
     private static final String ROOT_URL = "http://" + IP + "/web/app/";
-    public static final String URL_INSERT = ROOT_URL + "insert.php";
-    public static final String URL_QUERY = ROOT_URL + "query.php";
+
+    private static final String IP = "192.168.100.76"; // cambiará dependiendo de la IP que tenga su computadora, cuando se modifique no añadan cambios en el git pls
+    private static final String ROOT_URL = "http://" + IP + "/dashboard/web/app/";
+    
 }
